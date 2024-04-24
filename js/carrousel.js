a58200742792d6034f9c0316d6343230f4bde15a
Samuel Giroux <s_giroux@icloud.com>
2024-04-24 10:27:40 -0400

    s11c1 ajout des fonctions creer_carrousel_img() et creer_carrousel_radio()

--- js/carrousel.js
+++ js/carrousel.js
@@ -8,21 +8,51 @@
     console.log('carrousel__x' + carrousel__x.tagName);
     let galerie = document.querySelector('.galerie');
     console.log('galerie = ' + galerie.tagName);
-    let carrousel__figure = document.querySelector('.carrousel__figure');
 
-    /* Création dynamique d'une image du carrousel */
-    let carrousel__img = document.createElement('img');
-    carrousel__img.classList.add('carrousel__img');
+    let carrousel__figure = document.querySelector('.carrousel__figure');
 
     /* récupère la première image de la galerie */
     //let galerie__img = galerie.querySelector('img');
-
     /* pour créer une collection d'images de la galerie */
     let galerie__img = galerie.querySelectorAll('img');
     console.log(galerie__img);
+    let index = 0;
     for (const elm of galerie__img)
     {
+        creer_image_carrousel(index, elm);
+        creer_radio_carrousel(index);
+        index = index + 1;
+    }
+
+    /**
+     * Création d'un radio bouton du carrousel
+     * @param {*} index le numéro du radio
+     */
+    function creer_radio_carrousel(index) {
+        let carrousel__radio = document.createElement('input');
+        // class
+        // index
+        // type
+        // name
+        // ajouter dans carrousel__form
+        // ajouter un écouteur qui permettra de changer l'opacité de l'image " index "
+        //carrousel__img.children[index].style.opacity = 1;
+    }
+
+    /**
+     * Créer l'image du carrousel à partir de la galerie
+     * @param {*} index le numéro de l'image
+     * @param elm l'élément image de la galerie
+     */
+
+    function creer_image_carrousel(index, elm) {
         console.log(elm.src)
+        /* Création dynamique d'une image du carrousel */
+        let carrousel__img = document.createElement('img');
+        carrousel__img.src = elm.src;
+        carrousel__img.classList.add('carrousel__img');
+        carrousel__img.dataset.index = index;
+        carrousel__figure.appendChild(carrousel__img);
     }
 
     /*
@@ -40,6 +70,7 @@
     })
 /* Écouteur pour fermer la boîte modale */
     carrousel__x.addEventListener('mousedown', function(){
+        console.log('fermer carrousel')
         carrousel.classList.remove('carrousel--ouvrir'); // fermer le carrousel
     })
 
